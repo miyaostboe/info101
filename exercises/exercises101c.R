c1de6fdfe6996f32f4e440040093811910f9de3b
miyaostboe <miyaostboe@ucsb.edu>
2025-02-05 22:11:48 -0800

    info101 exercises

--- exercises/exercises101c.R
+++ exercises/exercises101c.R
@@ -3,12 +3,12 @@ rovs <- data.frame(
   model = c("Chasing M2 Pro", "Qysea Fifish Pro V6", "BlueROV2"),
   price_usd = c(3459, 2999, 6500)
 )
-rovs[???, ???]
+rovs[1, 1]
 
 
 # Pull out the model column from rovs using both square bracket and dollar sign
 # indexing
-
+rovs[rovs$model == "Chasing M2 Pro", ]
 
 # The file flowcytodata.csv contains artificial flow cytometry data, a technique
 # for counting phytoplankton and other single-cell organisms. Use the data from
@@ -19,11 +19,10 @@ flowcyto <- read.csv("flowcytodata.csv")
 # Move the comments below so they go with the correct line of code.
 
 # Pull out the abundances from the first cast at the first station
-
-# Pull out all rows where abundance was more than 400
-
-# Set all the values in abundance to NA for the first cast at the first station
-
-flowcyto[flowcyto$abundance > 400, ]
 flowcyto[flowcyto$station_id == 1 & flowcyto$cast == 1, 5]
+# Pull out all rows where abundance was more than 400
+flowcyto[flowcyto$abundance > 400, ]
+# Set all the values in abundance to NA for the first cast at the first station
 flowcyto[flowcyto$station_id == 1 & flowcyto$cast == 1, 5] <- NA
+
+
